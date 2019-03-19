6d354797f39b3fe07b5254a80ec876a40a7429f1
shindepavan <31682709+shindepavan@users.noreply.github.com>
2019-03-19 22:27:48 +0530

    Add files via upload

---
+++ test_environment.py
@@ -0,0 +1,19 @@
+from pytest import mark 
+
+@mark.xfail(reason = "The ENV is not QA")
+def test_environment_is_qa(app_config):
+    base_url = app_config.base_url
+    port = app_config.app_port
+    assert base_url == 'https://myqa-env.com'
+    assert port == 80
+
+def test_environment_is_dev(app_config):
+    base_url = app_config.base_url
+    port = app_config.app_port
+    assert base_url == 'https://mydev-env.com'
+    assert port == 8080
+@mark.skip
+def test_environment_is_staging(app_config):
+    base_url = app_config.base_url
+    port = app_config.app_port
+    assert base_url == 'staging'
